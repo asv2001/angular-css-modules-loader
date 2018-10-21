1a37b87c4ce50e4e75c1da3c5c5b4e6a807e9702
Anton Sinelnyk <asinelnyk@evolutiongaming.com>
2018-10-21 15:28:36 +0200

    Update replacer function

--- src/replacer.ts
+++ src/replacer.ts
@@ -14,8 +14,14 @@ function __replaceCssModules({ prefix, suffix }: ICssModulesParams) {
         let result: string = template;
         styles.forEach((style: object): void => {
             for (const [key, value] of Object.entries(style)) {
-                const matchRegExp: RegExp = new RegExp(`${__escapeRegExp(prefix)}\\b${__escapeRegExp(key)}\\b${__escapeRegExp(suffix)}`, "gm");
-                result = result.replace(matchRegExp, value);
+                const matchRegExp: RegExp = new RegExp(`(${__escapeRegExp(prefix)}\\b${__escapeRegExp(key)}\\b${__escapeRegExp(suffix)})["' \\n\\r\\]]`, "gm");
+                if (matchRegExp.test(result)) {
+                    matchRegExp.lastIndex = 0;
+                    let matches: RegExpExecArray | null;
+                    while ((matches = matchRegExp.exec(result)) !== null) {
+                        result = result.slice(0, matches.index) + value + result.slice(matches.index + matches[1].length);
+                    }
+                }
             }
         });
         return result;
